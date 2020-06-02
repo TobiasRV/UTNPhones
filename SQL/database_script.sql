6dff3db0bc0b70a383fcaaab2c69c97ae0ba3140
ChristianSoldano <soldanochristian@hotmail.com>
2020-06-02 17:02:39 -0300

    sql sp

--- SQL/database_script.sql
+++ SQL/database_script.sql
@@ -112,30 +112,6 @@ begin
 end //
 delimiter ;
 
--- STORED PROCEDURE QUE INSERTA UNA NUEVA CALL 
-delimiter // 
-create procedure sp_insert_call(pOriginNumber varchar(16), pDestinationNumber varchar(16), pDate datetime, pDuration int)
-begin
-  declare vOriginPrefix varchar(5);
-  declare vDestinationPrefix varchar(5);
-  declare vOriginNumber varchar(10);
-  declare vDestinationNumber varchar(10);
-  declare vOriginCityId int;
-  declare vDestinationCityId int;
-
-  set vOriginPrefix = substring_index(pOriginNumber,"-",1);
-  set vDestinationPrefix = substring_index(pDestinationNumber,"-",1);
-  set vOriginNumber = substring_index(pOriginNumber,"-",-1);
-  set vDestinationNumber = substring_index(pDestinationNumber,"-",-1);
-  set vOriginCityId = getCityIdByPrefix(vOriginPrefix);
-  set vDestinationCityId = getCityIdByPrefix(vDestinationPrefix);
-
-  insert into calls(id_origin_line, id_destination_line, call_date, call_duration)
-  values (getLineId(vOriginCityId,vOriginNumber), getLineId(vDestinationCityId,vDestinationNumber),pDate,pDuration);
-    
-end //
-delimiter ; 
-
 -- TRIGGER PARA EVITAR QUE UN NUMERO SE LLAME A SI MISMO
 delimiter //
 create trigger tbi_calls_prevent_self_calling before insert on calls for each row
@@ -146,17 +122,7 @@ begin
 end //
 delimiter ;
 
--- TRIGGER PARA OBTENER EL PRECIO DE UNA LLAMADA
-delimiter //
-create trigger tbi_calls_set_call_price before insert on calls for each row
-begin
-    set new.id_rate = getIdRate(new.id_origin_line, new.id_destination_line);
-    set new.call_price = getCallPrice(new.id_rate);
-end //
-delimiter ;
-
-
--- FUNCION PARA OBTENER EL ID_RATE BASADO EN ID_ORIGEN E ID_DESTINO
+-- FUNCION PARA OBTENER EL ID_RATE BASADO EN ID_ORIGEN_CITY E ID_DESTINO_CITY
 delimiter //
 create function getIdRate(pIdOriginLine int, pIdDestinationLine int)
 returns int
@@ -175,10 +141,53 @@ returns double
 reads sql data
 begin 
     declare vResult double;
+    select price_per_minute into vResult from rates where id_rate = pId_rate;
+    return vResult;
+end //
+delimiter ;
+
+-- FUNCION PARA OBTENER EL PRECIO DE UN RATE
+delimiter //
+create function getCallCost(pId_rate int)
+returns double
+reads sql data
+begin 
+    declare vResult double;
     select cost_per_minute into vResult from rates where id_rate = pId_rate;
     return vResult;
 end //
 delimiter ;
+
+-- STORED PROCEDURE QUE INSERTA UNA NUEVA CALL 
+delimiter // 
+create procedure sp_insert_call(pOriginNumber varchar(16), pDestinationNumber varchar(16), pDate datetime, pDuration int)
+begin
+  declare vOriginPrefix varchar(5);
+  declare vDestinationPrefix varchar(5);
+  declare vOriginNumber varchar(10);
+  declare vDestinationNumber varchar(10);
+  declare vOriginCityId int;
+  declare vDestinationCityId int;
+  declare vIdRate int;
+  declare vPrice double;
+  declare vCost double;
+
+  set vOriginPrefix = substring_index(pOriginNumber,"-",1);
+  set vDestinationPrefix = substring_index(pDestinationNumber,"-",1);
+  set vOriginNumber = substring_index(pOriginNumber,"-",-1);
+  set vDestinationNumber = substring_index(pDestinationNumber,"-",-1);
+  set vOriginCityId = getCityIdByPrefix(vOriginPrefix);
+  set vDestinationCityId = getCityIdByPrefix(vDestinationPrefix);
+
+  set vIdRate = getIdRate(vOriginCityId,vDestinationCityId);
+  set vPrice = pDuration * getCallPrice(vIdRate);
+  set vCost = pDuration * getCallCost(vIdRate);
+
+  insert into calls(id_origin_line, id_destination_line, call_date, id_rate, call_duration, call_cost, call_price)
+  values (getLineId(vOriginCityId,vOriginNumber), getLineId(vDestinationCityId,vDestinationNumber),pDate,vIdRate, pDuration,vCost, vPrice);
+    
+end //
+delimiter ; 
 
 -- INSERTS PROVINCES
 INSERT INTO provinces(province_name) values ("Buenos Aires"),("La Pampa"),("Cordoba"),("Santa Fe"),("Rio Negro"),("Corrientes"),("Entre Rios");
@@ -187,7 +196,7 @@ INSERT INTO provinces(province_name) values ("Buenos Aires"),("La Pampa"),("Cord
 INSERT INTO cities(city_name,id_province,prefix) values ("Mar del Plata",1,"223"),("Miramar",1,"2291"),("Necochea",1,"2262"),("Bahia Blanca",1,"291"),("Balcarce",1,"2266"),("Olavarria",1,"2284"),("Ciudad Autonoma de Buenos Aires",1,"011"),("La plata",1,"221"),("Cordoba Capital",3,"351"),("Santa Rosa",2,"2954");
 
 -- INSERTS USERS
-INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`role`) VALUES ("Melyssa","JSX77EKU0BJ","Hasad","Barnes",28623956,3,"client"),("Ignacia","QNI14CPA0LY","Quail","Dalton",42876066,9,"client"),("Hadley","IQQ01AEE8RZ","Vivian","Lynch",17086970,8,"client"),("Gloria","FDB42QHB8IH","Demetrius","Brennan",41571814,7,"client"),("Macon","ULK20VWV2DC","Damon","Salinas",15314357,7,"client"),("Ezekiel","PRA31VXO7OH","Haley","Allison",24202784,10,"client"),("Briar","CNT80TQJ4DL","Nomlanga","Alford",19115569,6,"client"),("Eric","PCG80MPR8EE","Hilary","Wynn",20803324,5,"client");
+INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`role`) VALUES ("siderjonas","pastrana","Christian","Soldano",40020327,1,"client"),("Soker","tobiasrv","Tobias","Rodriguez Viau",41216459,2,"client"),("Hadley","IQQ01AEE8RZ","Vivian","Lynch",17086970,8,"client"),("Gloria","FDB42QHB8IH","Demetrius","Brennan",41571814,7,"client"),("Macon","ULK20VWV2DC","Damon","Salinas",15314357,7,"client"),("Ezekiel","PRA31VXO7OH","Haley","Allison",24202784,10,"client"),("Briar","CNT80TQJ4DL","Nomlanga","Alford",19115569,6,"client"),("Eric","PCG80MPR8EE","Hilary","Wynn",20803324,5,"client");
 INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`role`) VALUES ("Maisie","FIK63URR0YI","Gray","Rollins",15964934,1,"client"),("Macaulay","TTJ86BEY7FI","Kaden","Mcclain",30595780,6,"client"),("Taylor","XLV26VKY5DB","Zena","May",15141960,10,"client"),("Isaac","KJY84VCT9XI","Abraham","Gilmore",42436632,4,"client"),("Asher","BUM04FXQ4UK","Mia","Tyson",23981647,5,"client"),("Rachel","ABS32BGY0TL","Lucius","Burke",27673033,1,"client"),("Hall","OGU32OFB7BL","Daria","Burnett",23478504,5,"client"),("Martin","CXF84DBZ4GM","Dahlia","Chambers",19944189,3,"client"),("Brianna","PDE12CQR3MN","Zelenia","Reyes",42484766,7,"client"),("Prescott","ROY90PPO4PW","Yuri","Charles",38944538,3,"client");
 INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`role`) VALUES ("Kieran","XLQ80FPF3NQ","Rhonda","Pierce",42811839,9,"client"),("Cairo","WXU31PZN5GA","Illiana","Conrad",39898544,4,"client"),("Price","ILL15LCF3UZ","Lewis","Williams",34182879,5,"client"),("Deirdre","NPT68VZD1ML","Carson","Harrell",22459768,1,"client"),("Edan","CJF10YBW5WQ","Chastity","Nicholson",43414428,10,"client"),("Aaron","IZC36PRX2AW","Halee","Pickett",28862585,1,"client"),("Hilel","UGG76EOS8MT","Tiger","Peterson",37854344,1,"client"),("Hollee","TAV42TPX6HS","Phelan","Rush",25436042,4,"client"),("Jarrod","HEA51UKU8KK","Dexter","Hensley",19006015,3,"client"),("Eaton","YYY95KOZ6ZF","Jenette","Thomas",27646304,2,"client");
 INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`role`) VALUES ("Vera","LDI35URH9FJ","Rinah","Conner",33472672,5,"client"),("Hermione","JVV52PJX5KB","Chester","Key",32162195,6,"client"),("Juliet","TGN10FSR9CR","Brock","Sweet",17112028,6,"client"),("Maggie","YHA91QNL3LK","Yuri","Vega",40371930,4,"client"),("Violet","AHB73VJD2ND","Blair","Lambert",32816169,8,"client"),("Aubrey","AAF58VOE0HL","Emerald","Fulton",18851271,10,"client"),("Stella","IJK13SAR8UK","Kennan","Whitley",25481781,9,"client"),("Bevis","MHZ07BWE3QQ","Louis","Wilkinson",34116407,6,"client"),("Signe","ZGC25BIB9PI","Hedwig","Leonard",18237951,2,"client"),("Brady","ENL37VXB9LP","Cruz","Coffey",29329606,3,"client");
@@ -202,7 +211,7 @@ INSERT INTO `users` (`username`,`password`,`name`,`lastname`,`dni`,`id_city`,`ro
 INSERT INTO rates(id_origin_city, id_destination_city, cost_per_minute, price_per_minute) values (1,1,6,5), (1,2,15,5), (1,3,9,5), (1,4,17,5), (1,5,15,5), (1,6,8,5), (1,7,10,5), (1,8,9,5), (1,9,11,5), (1,10,19,5), (2,1,11,5), (2,2,12,5), (2,3,14,5), (2,4,9,5), (2,5,17,5), (2,6,5,5), (2,7,12,5), (2,8,14,5), (2,9,8,5), (2,10,14,5), (3,1,6,5), (3,2,12,5), (3,3,10,5), (3,4,18,5), (3,5,6,5), (3,6,19,5), (3,7,14,5), (3,8,5,5), (3,9,13,5), (3,10,15,5), (4,1,15,5), (4,2,13,5), (4,3,19,5), (4,4,13,5), (4,5,15,5), (4,6,15,5), (4,7,19,5), (4,8,19,5), (4,9,19,5), (4,10,7,5), (5,1,10,5), (5,2,13,5), (5,3,17,5), (5,4,13,5), (5,5,14,5), (5,6,15,5), (5,7,10,5), (5,8,8,5), (5,9,8,5), (5,10,7,5), (6,1,8,5), (6,2,7,5), (6,3,14,5), (6,4,9,5), (6,5,8,5), (6,6,5,5), (6,7,5,5), (6,8,12,5), (6,9,7,5), (6,10,12,5), (7,1,7,5), (7,2,10,5), (7,3,19,5), (7,4,5,5), (7,5,8,5), (7,6,5,5), (7,7,10,5), (7,8,8,5), (7,9,7,5), (7,10,5,5), (8,1,15,5), (8,2,9,5), (8,3,7,5), (8,4,17,5), (8,5,13,5), (8,6,10,5), (8,7,5,5), (8,8,10,5), (8,9,19,5), (8,10,19,5), (9,1,7,5), (9,2,11,5), (9,3,11,5), (9,4,8,5), (9,5,5,5), (9,6,14,5), (9,7,6,5), (9,8,9,5), (9,9,11,5), (9,10,15,5), (10,1,7,5), (10,2,13,5), (10,3,12,5), (10,4,11,5), (10,5,16,5), (10,6,12,5), (10,7,5,5), (10,8,6,5), (10,9,9,5), (10,10,6,5);
 
 -- INSERTS PHONE_LINES 
-INSERT INTO `phone_lines` (`id_user`,`id_city`,`phone_number`,`line_type`,`status` ) VALUES (1,1,"5375920","RESIDENTIAL","ACTIVE"),(56,1,"9906290","MOBILE","ACTIVE"),(49,5,"4155176","MOBILE","ACTIVE"),(50,2,"9464855","MOBILE","ACTIVE"),(3,2,"3456156","RESIDENTIAL","ACTIVE"),(43,3,"7113169","RESIDENTIAL","ACTIVE"),(88,6,"4855135","MOBILE","ACTIVE"),(13,10,"5862927","MOBILE","ACTIVE"),(63,8,"1471419","MOBILE","ACTIVE"),(24,7,"9605421","RESIDENTIAL","ACTIVE");
+INSERT INTO `phone_lines` (`id_user`,`id_city`,`phone_number`,`line_type`,`status` ) VALUES (1,1,"6363325","MOBILE","ACTIVE"),(2,2,"412505","MOBILE","ACTIVE"),(49,5,"4155176","MOBILE","ACTIVE"),(50,2,"9464855","MOBILE","ACTIVE"),(3,2,"3456156","RESIDENTIAL","ACTIVE"),(43,3,"7113169","RESIDENTIAL","ACTIVE"),(88,6,"4855135","MOBILE","ACTIVE"),(13,10,"5862927","MOBILE","ACTIVE"),(63,8,"1471419","MOBILE","ACTIVE"),(24,7,"9605421","RESIDENTIAL","ACTIVE");
 INSERT INTO `phone_lines` (`id_user`,`id_city`,`phone_number`,`line_type`,`status` ) VALUES (39,2,"3730060","RESIDENTIAL","ACTIVE"),(25,10,"8118949","RESIDENTIAL","ACTIVE"),(88,9,"9676506","MOBILE","ACTIVE"),(31,2,"4678017","MOBILE","ACTIVE"),(93,10,"3027481","RESIDENTIAL","ACTIVE"),(40,6,"3415217","RESIDENTIAL","ACTIVE"),(86,7,"9788484","RESIDENTIAL","ACTIVE"),(79,3,"3655135","MOBILE","ACTIVE"),(52,4,"5311449","RESIDENTIAL","ACTIVE"),(5,4,"6191557","RESIDENTIAL","ACTIVE");
 INSERT INTO `phone_lines` (`id_user`,`id_city`,`phone_number`,`line_type`,`status` ) VALUES (92,7,"5628958","RESIDENTIAL","ACTIVE"),(69,5,"2567757","RESIDENTIAL","ACTIVE"),(64,7,"6975705","RESIDENTIAL","ACTIVE"),(30,9,"2221318","RESIDENTIAL","ACTIVE"),(5,9,"3138217","MOBILE","ACTIVE"),(96,7,"7603644","RESIDENTIAL","ACTIVE"),(75,8,"6232279","RESIDENTIAL","ACTIVE"),(67,3,"3692339","MOBILE","ACTIVE"),(87,7,"6997606","MOBILE","ACTIVE"),(13,4,"1377869","RESIDENTIAL","ACTIVE");
 INSERT INTO `phone_lines` (`id_user`,`id_city`,`phone_number`,`line_type`,`status` ) VALUES (44,1,"2090542","RESIDENTIAL","ACTIVE"),(71,10,"4531090","RESIDENTIAL","ACTIVE"),(25,8,"5226104","MOBILE","ACTIVE"),(66,4,"1194363","RESIDENTIAL","ACTIVE"),(69,3,"6759301","MOBILE","ACTIVE"),(30,3,"9101212","RESIDENTIAL","ACTIVE"),(11,2,"7608900","RESIDENTIAL","ACTIVE"),(38,5,"4669203","MOBILE","ACTIVE"),(33,8,"1732779","RESIDENTIAL","ACTIVE"),(80,3,"2046194","MOBILE","ACTIVE");
@@ -220,4 +229,4 @@ insert into bills (id_line,total_production_cost, total_price, issue_date, expir
 insert into bills (id_line,total_production_cost, total_price, issue_date, expiration_date) values (1,50,300,"2020-05-01","2020-06-01");
 
 -- INSERT CALL 
-call sp_insert_call("223-5375920","223-9906290",now(),70);
+call sp_insert_call("223-6363325","2291-412505", now(), 300);
